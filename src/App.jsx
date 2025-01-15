85b80502d1abc82c5c05979b8c36096b1371e487
NemesisUA <display.n0ne.01.gmail.com>
2025-01-15 11:26:49 +0200

    feat: add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
 
-      <section className="bg-blue-500 min-h-screen">test</section>
+      <About />
     </main>
   )
 }
